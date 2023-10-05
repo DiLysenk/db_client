8f3dcfac7de7febe24520c9706b7e42589e5faee
Lysenko Dmitry <lysenkodmitry@MacBook-Air-Lysenko.local>
2023-10-06 01:11:31 +0300

    fix marks

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+REQUIRES = [
+    'requests',
+    'structlog',
+    'curlify',
+    'allure-pytest'
+]
+
+
+
+setup(
+    name='db_client',
+    version='0.1',
+    packages=['db_client'],
+    url='https://github.com/DiLysenk/restclient_dl',
+    license='MIT',
+    author='lysenkodmitry',
+    author_email='-',
+    install_requires=REQUIRES,
+    description='db_client ohh'
+)
